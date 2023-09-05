ee386ed0684c5b5a812e4733e99f391cde1c4475
alexrexjh <144179943+alexrexjh@users.noreply.github.com>
2023-09-05 16:07:49 -0600

    practica 1 05 de septiembre

---
+++ p1_1s21{,java.java
@@ -0,0 +1,11 @@
+class p1_1s21{
+	public static void main(String[] args) {
+	system.out.println("datos personales");
+	nombre alexander 
+	edad 18
+	correo alumnolexœgmail.com 
+	aficion taekwoando 
+	color favorito el negro 
+
+	}
+}
