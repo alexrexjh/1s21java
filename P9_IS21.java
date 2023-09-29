ebc4367b3abc0e55c4f405abec68653746389bf7
alexrexjh <144179943+alexrexjh@users.noreply.github.com>
2023-09-28 22:01:33 -0600

    Add files via upload

---
+++ P9_IS21.java
@@ -0,0 +1,23 @@
++ import java.util.Scanner;
++
++ public class p9_1s21{
++         public static  void main(String[] args) {
++                Scanner teclado = new Scanner(System.in);
++                String operacion;
++                System.out.println("Que tipo de operacion deseas realezar");
++                System.out.println("escriba Suma,Resta,divicion,multiplicacion");
++                operacion = teclado.nextLine();
++
++                if (operacion.equals("suma")) {
++                       System.out.println("Haremos una suma");
++                  } else if (operacion.out.equals)("Resta")){ 
++                      System.out.println("Haremos una resta");
++                  } else if (operacion.equals("divicion")){
++                      System.out.println("Haremos una divicion");
++                  } else if (operacion.equals("multiplicacion")){
++                      System.out.println("Haremos una multiplicacion");
++                  } else { 
++                          System.out.println("No existe la operacion");
++                  }
++            )
++    )
